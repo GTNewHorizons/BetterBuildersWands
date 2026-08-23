db7d8005ba0ac9bda80217bc1c6c0a95df6ee410
Guillaume Mercier <17766417+C0bra5@users.noreply.github.com>
2026-08-23 07:31:59 -0400

    Add safe guards for divide by zero.

--- src/main/java/portablejim/bbw/core/WandWorker.java
+++ src/main/java/portablejim/bbw/core/WandWorker.java
@@ -303,8 +303,10 @@ public class WandWorker {
 
         boolean isCreative = playerShim.isCreative();
         Point3d[] blockPoss = blockPosList.toArray(new Point3d[0]);
-        needItem.stackSize = blockPoss.length * sourceItems.stackSize;
-        int takeFromInventory = playerShim.useItem(needItem, isNBTSensitive) / sourceItems.stackSize;
+        // stack size shouldn't be below zero but better safe than sorry.
+        int amountToConsume = Math.max(1, sourceItems.stackSize);
+        needItem.stackSize = blockPoss.length * amountToConsume;
+        int takeFromInventory = playerShim.useItem(needItem, isNBTSensitive) / amountToConsume;
 
         for (int i = 0; i < takeFromInventory; ++i) {
             Point3d blockPos = blockPoss[i];
